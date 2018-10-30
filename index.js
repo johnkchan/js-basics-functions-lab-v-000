8e299eb665abf2bc34f0a13db5e445cdbabe257a
Jonathan Chan <johnkchan252@gmail.com>
2018-10-30 02:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function calculatesFarePrice(distance){
   if (distanceTravelledInFeet(distance) < 400){
     return 0;
   } else if (distanceTravelledInFeet(distance) >= 400 && distanceTravelledInFeet(distance) <= 2000){
+    return (distanceTravelledInFeet - 400) * .02;
+  } else if (distanceTravelledInFeet(distance) >= 400 && distanceTravelledInFeet(distance) <= 2000){
     
   }
 }
